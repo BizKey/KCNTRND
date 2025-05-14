57e2bffd93f0611d964ea5d1a623ec9b25705baa
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2025-05-15 00:24:06 +0300

    .

--- src/main.py
+++ src/main.py
@@ -2569,7 +2569,7 @@ class KCN:
                         current_price,
                         avail_tokens,
                     )
-                    for _ in self.logger_info(order_params)
+                    for _ in self.post_api_v1_stop_order(order_params)
                 ):
                     case Ok(_):
                         pass
